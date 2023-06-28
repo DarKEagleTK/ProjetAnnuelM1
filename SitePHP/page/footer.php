8eb2f9d3d8b291cf4d4048b2a1d57a345e490fd6
Dimitri <dimitrilepoutre7@gmail.com>
2023-06-28 23:44:13 +0200

    Ajout information site web

--- SitePHP/page/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- footer.php -->
-<footer class="bg-light text-center text-lg-start mt-auto">
-  <div class="text-center p-3">
-    © 2021 Copyright:
-    <a class="text-dark" href="DarKEagle">OpenClassrooms</a>
-  </div>
-</footer>
