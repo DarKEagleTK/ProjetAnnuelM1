df426e158cef036b07d60acba568f304c4ccdd0d
Raphaël Bourreau <43635207+Shira518@users.noreply.github.com>
2023-07-09 11:26:48 +0200

    part2

---
+++ script/windows/Set_dns_ttl.ps1
@@ -0,0 +1,11 @@
+param
+(
+    [string]$srvName,
+    [string]$domain,
+    [string]$ttl
+)
+
+$OldObj = Get-DnsServerResourceRecord -Name "$srvName" -ZoneName "$domain" -RRType "A"
+$NewObj = [ciminstance]::new($OldObj)
+$NewObj.TimeToLive = [System.TimeSpan]::FromSeconds($ttl)
+Set-DnsServerResourceRecord -NewInputObject $NewObj -OldInputObject $OldObj -ZoneName "$domain" -PassThru
