15a85abd40f004a30724eaf32660f0e126d237a5
LaZyLOu <louise-anne_dayao@hotmail.fr>
2023-07-12 21:52:21 +0200

    almost final bash script web

--- script/linux/web/create_site.sh
+++ script/linux/web/create_site.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
 
-#Demande des variables
-#echo "Variables : NAMESPACE RADICAL DEPLOY_DIR NB_REPLICAS"
-#read -s -p "Votre namespace ?" NAMESPACE; echo
-#read -n 15 -s -p  "Le radical de nommage ?" RADICAL; echo
-#read -s -p "Nom du dossier ?" DEPLOY_DIR; echo
-#read -s -p "Nombre de replicas ?" NB_REPLICAS; echo
-#echo "Variables : $NAMESPACE $RADICAL $DEPLOY_DIR $NB_REPLICAS"
-
+variable=$1
 #Récupération des variables (paramètre $variable)
 RADICAL=$(echo $variable| awk -F'www.' '{print $2}' | awk -F'.' '{print $1}')
-
-#si le radical fait + de 15char ou a des caractères spéciaux
+echo $RADICAL
 
 #Création du dossier de deploiement
-mkdir $RADICAL\-folder && cd $RADICAL-folder
+mkdir $RADICAL-folder && cd $RADICAL-folder/
+
+#Copie du dossier ou zip dans /tmp/ dans ./
+if [ -f /tmp/$RADICAL.zip ]; then
+    unzip /tmp/$RADICAL.zip -d /home/admuser/$RADICAL-folder
+else
+    cp -r /tmp/$RADICAL /home/admuser/$RADICAL-folder/
+fi
 
 #Création du nom de l'espace pour le conteneur dédié
-kubectl create namespace $RADICAL\-ns
+kubectl create namespace $RADICAL-ns
 
 #Création de la ConfigMap selon le dossier spécifié
-kubectl create configmap $RADICAL\-cm --from-file=./
+kubectl create configmap $RADICAL-cm --from-file=/home/admuser/$RADICAL-folder/ -n=$RADICAL-ns
 
 #Création du fichier deployment.yaml (qui contiendra également le service.yaml)
 cp /home/admuser/template_deployment.yaml deployment.yaml
@@ -29,4 +28,4 @@ cp /home/admuser/template_deployment.yaml deployment.yaml
 sed -i -e "s/RADICAL/$RADICAL/g" deployment.yaml
 
 #Application du deployment.yaml dans le NAMESPACE spécifié
-kubectl apply -n $RADICAL\-ns -f "./deployment.yaml"
+kubectl apply -n $RADICAL-ns -f "./deployment.yaml"
--- script/linux/web/supp_site.sh
+++ script/linux/web/supp_site.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 # Variables (paramètre $variable)
+variable=$1
 RADICAL=$(echo $variable| awk -F'www.' '{print $2}' | awk -F'.' '{print $1}')
 
 #Désinstallation de déploiement
-kubectl delete -n $RADICAL\-ns deployment $RADICAL\-deployment
-kubectl delete -n $RADICAL\-ns service $RADICAL\-service
-kubectl delete -n $RADICAL\-ns cm $RADICAL\-cm
-kubectl delete ns $RADICAL\-ns
+kubectl delete -n $RADICAL-ns deployment $RADICAL-deployment
+kubectl delete -n $RADICAL-ns service $RADICAL-service
+kubectl delete -n $RADICAL-ns cm $RADICAL-cm
+kubectl delete ns $RADICAL-ns
 
 #Suppression du dossier de conteneur
-sudo rm -r $RADICAL\-folder
+sudo rm -r $RADICAL-folder/
+
