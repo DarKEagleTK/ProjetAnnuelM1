cc22662a984b17ac56ef098e48c3587f8d6506e4
Raphaël Bourreau <43635207+Shira518@users.noreply.github.com>
2023-07-12 21:20:24 +0200

    En tete pour connexion

--- script/windows/Add_dns_aaaa.ps1
+++ script/windows/Add_dns_aaaa.ps1
@@ -1,7 +1,4 @@
-param
-(
-    [string]$srvName,
-    [string]$domain
-)
-
-Add-DnsServerResourceRecordAAAA -Name "$srvName" -ZoneName "$domain" -AllowUpdateAny -TimeToLive 01:00:00
+$securePassword = ConvertTo-SecureString -String "Toto12345" -AsPlainText -Force
+$id = "TGT\administrateur"
+$credential = New-Object System.Management.Automation.PSCredential($id, $securePassword)
+Invoke-Command -ComputerName 10.1.0.12 -Credential $credential -ScriptBlock {param([string]$srvName, [string]$domain) Add-DnsServerResourceRecordAAAA -Name "$srvName" -ZoneName "$domain" -AllowUpdateAny -TimeToLive 01:00:00}
